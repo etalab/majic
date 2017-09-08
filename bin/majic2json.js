10b8e639430b0902ab5d28cd6256641e05542e7c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-09-09 00:57:25 +0200

    Use newly exported module

--- bin/majic2json.js
+++ bin/majic2json.js
@@ -3,10 +3,9 @@
 
 const {serialize} = require('ndjson')
 const split = require('split2')
-const parse = require('../lib/parse')
+const {parse} = require('../')
 
 process.stdin
-  .pipe(split())
   .pipe(parse())
   .pipe(serialize())
   .pipe(process.stdout)
