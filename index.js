ce5ae9a09d82bad391357fe56195ed8e33f0645e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-04 16:06:52 +0100

    Move parse stream

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-'use strict'
-
-const split = require('split2')
-const {createGunzip} = require('gunzip-stream')
-const pumpify = require('pumpify')
 const parse = require('./lib/parse')
 
-exports.parse = function (options) {
-  return pumpify.obj(
-    createGunzip(),
-    split(),
-    parse(options)
-  )
-}
+module.exports = {parse}
